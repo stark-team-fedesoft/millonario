57de2ec1f24c57dbd90339eb6162c093296487b2
Miguel Arenas <miguel.arenas@sitidoctor.com>
2021-07-29 08:41:28 -0500

    The data structure is in building

---
+++ assets/js/data.js
@@ -0,0 +1,11 @@
+window.dataQues = {
+    questions : [
+        {
+            id       : 1,
+            question : '¿Quién conquisto América?',
+            options  : [
+                { id: 'A', answer : 'Simón Bolivar', }
+            ]
+        }
+    ]
+}
