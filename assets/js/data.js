aa04c15f4c75e4c1c9e6c3976d998702654bea54
Miguel Arenas <miguel.arenas@sitidoctor.com>
2021-07-29 09:03:44 -0500

    obj json preguntas proyecto

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,11 +1,38 @@
 window.dataQues = {
     questions : [
         {
-            id       : 1,
-            question : '¿Quién conquisto América?',
-            options  : [
-                { id: 'A', answer : 'Simón Bolivar', }
-            ]
-        }
+            id        : 1,
+            question  : '¿Quién conquisto América?',
+            options   : [
+                { id: 'A', answer : 'Simón Bolivar' },
+                { id: 'B', answer : 'George Washington' },
+                { id: 'C', answer : 'Cristobal Colón' },
+                { id: 'D', answer : 'Bad Bunny' },
+            ],
+            value     : 200000.00,
+            correctOp : 'C',
+            access    : true,
+        },
+        {
+            id        : 2,
+            question  : '¿Quién inventó el primer vehículo de la historia?',
+            options   : [
+                { id: 'A', answer : 'Henry Ford' },
+                { id: 'B', answer : 'Karl Benz' },
+                { id: 'C', answer : 'Michael Schumacher' },
+                { id: 'D', answer : 'Louis Chevrolet' },
+            ],
+            value     : 500000.00,
+            correctOp : 'B',
+            access    : false,
+        },
     ]
 }
+
+const questions = window.dataQues.questions;
+
+const q2 = questions.find( q => {
+    return q.id === 2; 
+});
+
+console.log(q2);
